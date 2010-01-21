55d135c77697c8cb6a776e2f9c600b4f73b44128
Caleb Clausen <coatl@inforadical.net>
2010-01-20 17:51:32 -0800

    example dialect which allows c comments

---
+++ lib/polyglot/dialect/ccomment.rb
@@ -0,0 +1,28 @@
+module Polyglot
+  class Dialect
+
+    class CComment<RedParseDialect
+      def initialize
+        super(LexerHack)
+      end
+
+      module LexerHack
+        def regex_or_div ch
+          if read(2)=='/*'
+            start=input_position-2
+            contents=scan_until('*/')
+            @linenum+=contents.count("\n")
+            RubyLexer::IgnoreToken.new(contents,start)
+          else
+            move -2
+            super
+          end
+        end
+      end
+
+      Polyglot.register(:ccomment,self)
+    end
+
+  end
+end
+
